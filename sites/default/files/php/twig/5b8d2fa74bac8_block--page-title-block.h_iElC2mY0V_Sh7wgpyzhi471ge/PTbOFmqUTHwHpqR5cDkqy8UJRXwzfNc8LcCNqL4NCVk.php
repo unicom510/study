97ef637bdba693aa63e42cc6468ecd11958df5fa
unicom510 <o.tarasiuk@internetdevels.com>
2018-09-03 16:57:15 +0300

    first version

---
+++ sites/default/files/php/twig/5b8d2fa74bac8_block--page-title-block.h_iElC2mY0V_Sh7wgpyzhi471ge/PTbOFmqUTHwHpqR5cDkqy8UJRXwzfNc8LcCNqL4NCVk.php
@@ -0,0 +1,77 @@
+<?php
+
+/* themes/conrib/bootstrap/templates/block/block--page-title-block.html.twig */
+class __TwigTemplate_d38caffa457c98d8217088ac1c489b180cefb99fcb02d857081b62f168465d66 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 9
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/conrib/bootstrap/templates/block/block--page-title-block.html.twig", 9);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/conrib/bootstrap/templates/block/block--page-title-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  11 => 9,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/conrib/bootstrap/templates/block/block--page-title-block.html.twig", "/var/www/study/themes/conrib/bootstrap/templates/block/block--page-title-block.html.twig");
+    }
+}
